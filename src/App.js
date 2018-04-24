9e400e7934a48eaf17af504817c0b67354ae1d42
James Almeida <james.almeida.ti@gmail.com>
2018-04-24 20:14:17 -0300

    Adiciona primeiro componente - pagina Home - section bg-background, caixa de pesquisa

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Home from './components/home/Home.js'
+import './css/Auxiliares.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Home/>
+    );
+  }
+}
+
+export default App;
