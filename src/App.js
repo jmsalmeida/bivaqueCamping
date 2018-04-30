cdf981d48bfc1106c594dcd7f6ec3b9a7a23af95
James Almeida <james.almeida.ti@gmail.com>
2018-04-30 16:36:15 -0300

    Correção caminho App.css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Pesquisa from './components/home/Pesquisa.js'
 import CampingRecomendado from './components/home/CampingRecomendado.js';
-import './css/App.css';
+import './App.css';
 
 import camping from './components/home/images/camp.jpg';
 import Campings from './components/PesquisaCampings/Campings.js';
@@ -15,8 +15,6 @@ class App extends Component {
         <Route path="/" exact component={ () => <HomePage /> } />
         <Route path="/camping-search" exact component={() => <Campings />} />
       </div>
-
-
     );
   }
 }
