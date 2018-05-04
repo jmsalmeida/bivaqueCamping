3fe9affcc76146bbd7e29a22f1d801d8366d31e5
James Almeida <james.almeida.ti@gmail.com>
2018-05-04 12:19:41 -0300

    Adiciona component ButtonContato e injeta em InfoCampingPage

--- src/pages/InfoCampingPage.js
+++ src/pages/InfoCampingPage.js
@@ -5,11 +5,12 @@ import './css/InfoCampingPage.css';
 import CarouselFotosCamping from '../components/CarouselFotosCamping/CarouselFotosCamping';
 import SobreCamping from '../components/SobreCamping/SobreCamping';
 import TituloCamping from '../components/TituloCamping/TituloCamping';
-import InfoCampingIcon from '../components/InfoCampingImage/InfoCampingIcon';
+import InfoCampingIcon from '../components/InfoCampingIcon/InfoCampingIcon';
 import Regras from '../components/Regras/Regras';
 import Endereco from '../components/Endereco/Endereco';
 // Icons
 import wireless from '../icons/wifi.svg'
+import ButtonContato from '../components/ButtonContato/ButtonContato';
 
 
 class InfoCampingPage extends Component {
@@ -24,32 +25,35 @@ class InfoCampingPage extends Component {
                     <SobreCamping sobre={sobreTxt} />
                     <div className="info">
                         <div className="left-column">
-                        <h3>Comodidades</h3>
+                            <h3>Comodidades</h3>
                             <div className="comod-icons">
-                                <InfoCampingIcon icon={wireless}/>
-                                <InfoCampingIcon icon={wireless}/>
-                                <InfoCampingIcon icon={wireless}/>
+                                <InfoCampingIcon icon={wireless} />
+                                <InfoCampingIcon icon={wireless} />
+                                <InfoCampingIcon icon={wireless} />
                             </div>
                             <h3>Regras</h3>
                             <div>
-                                <Regras regras="Regra"/>
-                                <Regras regras="Regra"/>
-                                <Regras regras="Regra"/>
+                                <Regras regras="Regra" />
+                                <Regras regras="Regra" />
+                                <Regras regras="Regra" />
                             </div>
                         </div>
                         <div className="y-bar"></div>
                         <div className="right-column">
-                        <h3>Proximidades</h3>
+                            <h3>Proximidades</h3>
                             <div className="comod-icons">
-                                <InfoCampingIcon icon={wireless}/>
-                                <InfoCampingIcon icon={wireless}/>
-                                <InfoCampingIcon icon={wireless}/>
+                                <InfoCampingIcon icon={wireless} />
+                                <InfoCampingIcon icon={wireless} />
+                                <InfoCampingIcon icon={wireless} />
                             </div>
                             <h3>Como chegar</h3>
                             <div>
-                                <Endereco map="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.2918022210447!2d-45.60759744980426!3d-23.772621184502714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94d27fddd9bdb17f%3A0x38ef438400b23349!2sPorongaba+Camping!5e0!3m2!1spt-BR!2sbr!4v1525439733474" endereco="Rua Dom Gabriel Paulino Bueno Couto, nº 19 Itu-SP"/>
+                                <Endereco map="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.2918022210447!2d-45.60759744980426!3d-23.772621184502714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94d27fddd9bdb17f%3A0x38ef438400b23349!2sPorongaba+Camping!5e0!3m2!1spt-BR!2sbr!4v1525439733474" endereco="Rua Dom Gabriel Paulino Bueno Couto, nº 19 Itu-SP" />
                             </div>
                         </div>
+                    </div>
+                    <div className="btn-contato">
+                        <ButtonContato/>
                     </div>
                 </div>
             </div>
