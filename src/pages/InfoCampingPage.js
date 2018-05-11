94c61af4a98319d8692eb9b205f576f0f291f50a
James Almeida <james.almeida.ti@gmail.com>
2018-05-11 15:30:01 -0300

    Adiciona sections para espacamento, ajustes espacamentos

--- src/pages/InfoCampingPage.js
+++ src/pages/InfoCampingPage.js
@@ -27,43 +27,45 @@ class InfoCampingPage extends Component {
         return (
             <div className="info-page">
                 <CarouselFotosCamping />
-                <div className="caixa">
-                    <TituloCamping titulo="Camping das pedras" cidade="Itu - São Paulo" />
-                    <h3>O espaço</h3>
-                    <SobreCamping sobre={sobreTxt} />
-                    <div className="info">
-                        <div className="left-column">
-                            <h3>Comodidades</h3>
-                            <div className="comod-icons">
-                                <InfoCampingIcon icon={wireless} />
-                                <InfoCampingIcon icon={notebook} />
-                                <InfoCampingIcon icon={periodo} />
+                <section className="sobre-camping">
+                    <div className="caixa">
+                        <TituloCamping titulo="Camping das pedras" cidade="Itu - São Paulo" />
+                        <h3>O espaço</h3>
+                        <SobreCamping sobre={sobreTxt} />
+                        <div className="info">
+                            <div className="left-column">
+                                <h3>Comodidades</h3>
+                                <div className="comod-icons">
+                                    <InfoCampingIcon icon={wireless} />
+                                    <InfoCampingIcon icon={notebook} />
+                                    <InfoCampingIcon icon={periodo} />
+                                </div>
+                                <h3>Regras</h3>
+                                <div>
+                                    <Regras regras="Regra" />
+                                    <Regras regras="Regra" />
+                                    <Regras regras="Regra" />
+                                </div>
                             </div>
-                            <h3>Regras</h3>
-                            <div>
-                                <Regras regras="Regra" />
-                                <Regras regras="Regra" />
-                                <Regras regras="Regra" />
+                            <div className="y-bar"></div>
+                            <div className="right-column">
+                                <h3>Proximidades</h3>
+                                <div className="comod-icons">
+                                    <InfoCampingIcon icon={mapa} />
+                                    <InfoCampingIcon icon={posto} />
+                                    <InfoCampingIcon icon={market} />
+                                </div>
+                                <h3>Como chegar</h3>
+                                <div>
+                                    <Endereco map="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.2918022210447!2d-45.60759744980426!3d-23.772621184502714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94d27fddd9bdb17f%3A0x38ef438400b23349!2sPorongaba+Camping!5e0!3m2!1spt-BR!2sbr!4v1525439733474" endereco="Rua Dom Gabriel Paulino Bueno Couto, nº 19 Itu-SP" />
+                                </div>
                             </div>
                         </div>
-                        <div className="y-bar"></div>
-                        <div className="right-column">
-                            <h3>Proximidades</h3>
-                            <div className="comod-icons">
-                                <InfoCampingIcon icon={mapa} />
-                                <InfoCampingIcon icon={posto} />
-                                <InfoCampingIcon icon={market} />
-                            </div>
-                            <h3>Como chegar</h3>
-                            <div>
-                                <Endereco map="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.2918022210447!2d-45.60759744980426!3d-23.772621184502714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94d27fddd9bdb17f%3A0x38ef438400b23349!2sPorongaba+Camping!5e0!3m2!1spt-BR!2sbr!4v1525439733474" endereco="Rua Dom Gabriel Paulino Bueno Couto, nº 19 Itu-SP" />
-                            </div>
+                        <div className="btn-contato">
+                            <ButtonContato/>
                         </div>
                     </div>
-                    <div className="btn-contato">
-                        <ButtonContato/>
-                    </div>
-                </div>
+                </section>
             </div>
         );
     }
