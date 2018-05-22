c7ee2abfcb32865954096a99debfbcc5c88a50b4
James Almeida <james.almeida.ti@gmail.com>
2018-05-22 19:18:00 -0300

    Adicionar botao 'mais fotos'

--- src/components/CarouselFotosCamping/CarouselFotosCamping.js
+++ src/components/CarouselFotosCamping/CarouselFotosCamping.js
@@ -5,13 +5,7 @@ class CarouselFotosCamping extends Component {
     render() {
         return (
             <div className="carousel-banner">
-                {/* <div className="setas">
-                    <span className="setas-banner"><i class="fas fa-angle-left"></i></span>
-                    <span className="setas-banner"><i class="fas fa-angle-right"></i></span>
-                </div> */}
-                {/* <div>
-                    <strong>Veja mais fotos</strong>
-                </div> */}
+                <button>mais fotos</button>
             </div>
         );
     }
