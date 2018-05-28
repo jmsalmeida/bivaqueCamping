cad6177aade94c6a52a0adbca76c079bdae0df43
James Almeida <james.almeida.ti@gmail.com>
2018-05-28 18:25:27 -0300

    Adicionar component IconeDicas -workInProgress

---
+++ src/components/IconeDicas/IconeDicas.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './IconeDicas.css';
+
+class IconeDicas extends Component {
+    render(){
+        let dica = this.props.dica;
+        let titulo = this.props.titulo;
+
+        return(
+            <div className="icone-dicas">
+                <img src={dica}/>
+                <h3>{titulo}</h3>
+            </div>
+        );
+    }
+}
+
+export default IconeDicas;
--- src/pages/DicasPage.js
+++ src/pages/DicasPage.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import './css/DicasPage.css';
 import Modal from '../components/Modal/Modal';
+import IconeDicas from '../components/IconeDicas/IconeDicas';
+
+// Images
+import arvore from './images/icone-dicas/arvore.svg';
+import seguranca from './images/icone-dicas/seguranca.svg';
+import bagagem from './images/icone-dicas/bagagem.svg';
 
 class DicasPage extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <div className="dicas">
-                <Modal/>
+                <div className="caixa">
+                    <h2>Guia do Campista</h2>
+                    <p>Olá, seja bem vindo!
+Aqui selecionamos algumas dicas para você relembrar ou saber o que é necessário para uma boa viagem sem nenhuma surpresa indesejada no meio da sua viagem.</p>
+                    <div className="icones">
+                        <IconeDicas dica={arvore} titulo={'Árvore'}/>
+                        <IconeDicas dica={seguranca} titulo={'Árvore'}/>
+                        <IconeDicas dica={bagagem} titulo={'Árvore'}/>
+                    </div>
+                </div>
             </div>
         );
     }
