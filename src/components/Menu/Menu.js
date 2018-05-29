8d46f4465b039bb1f6d7d915f1a49f09e86b0093
James Almeida <james.almeida.ti@gmail.com>
2018-05-28 22:50:18 -0300

    Adicionar DicasPage workInProgress

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Menu.css';
-
+import { Link } from 'react-router-dom';
 // Images
 import logo from './images/bivaque-logo.png';
 
@@ -12,7 +12,7 @@ class Menu extends Component {
                     <img src={logo}/>
                     <ul>
                         <a>Cadastre seu camping</a>
-                        <a>Dicas</a>
+                        <Link to="/dicas"><a>Dicas</a></Link>
                         <a onClick={this.props.alternarModal}>Entrar</a>
                     </ul>
                 </nav>
--- src/pages/DicasPage.js
+++ src/pages/DicasPage.js
@@ -14,12 +14,12 @@ class DicasPage extends Component {
             <div className="dicas">
                 <div className="caixa">
                     <h2>Guia do Campista</h2>
-                    <p>Olá, seja bem vindo!
-Aqui selecionamos algumas dicas para você relembrar ou saber o que é necessário para uma boa viagem sem nenhuma surpresa indesejada no meio da sua viagem.</p>
-                    <div className="icones">
-                        <IconeDicas dica={arvore} titulo={'Árvore'}/>
-                        <IconeDicas dica={seguranca} titulo={'Árvore'}/>
-                        <IconeDicas dica={bagagem} titulo={'Árvore'}/>
+                    <p>Olá, seja bem vindo!<br />
+                        Aqui selecionamos algumas dicas para você relembrar ou saber o que é necessário para uma boa viagem sem nenhuma surpresa indesejada no meio da sua viagem.</p>
+                    <div className="single-icon">
+                        <IconeDicas dica={bagagem} titulo={'Bagagem'} />
+                        <IconeDicas dica={seguranca} titulo={'Segurança'} />
+                        <IconeDicas dica={arvore} titulo={'Meio Ambiente'} />
                     </div>
                 </div>
             </div>
