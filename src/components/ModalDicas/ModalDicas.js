84fc8d80704cf2ca0b3bed767994e5781a41e12b
James Almeida <james.almeida.ti@gmail.com>
2018-05-30 15:05:27 -0300

    Adicionar itens fixos na modal

--- src/components/ModalDicas/ModalDicas.js
+++ src/components/ModalDicas/ModalDicas.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
 import './ModalDicas.css';
+import IconeDicas from '../IconeDicas/IconeDicas';
+
+
+// Images
+import arvore from '../../pages/images/icone-dicas/arvore.svg';
+import seguranca from '../../pages/images/icone-dicas/seguranca.svg';
+import bagagem from '../../pages/images/icone-dicas/bagagem.svg';
 
 class ModalDicas extends Component {
-    render(){
+    render() {
         let modalDicasCSS = 'modal-dicas';
 
-        return(
-            <div className={modalDicasCSS}>
-
+        return (
+            <div>
+                <div className={modalDicasCSS}>
+                    <IconeDicas dica={bagagem} />
+                    <h3>Nome da Modal que abriu</h3>
+                    <div className="itens-dicas">
+                        <ul>
+                            <li>aqui</li>
+                            <li>pode</li>
+                            <li>ser vários tópicos</li>
+                            <li>e pode rolar um scroll</li>
+                            <li>mas pode deixar sem scroll</li>
+                            <li>e rolar as bolinhas que estão em baixo</li>
+                        </ul>
+                        <div className="bolinhas-navegar">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div> 
+                    </div>
+                     
+                </div>
             </div>
         );
     }
