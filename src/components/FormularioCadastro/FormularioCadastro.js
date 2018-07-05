8e11639b04d30426772276d976b5203c720a7191
James Almeida <james.almeida.ti@gmail.com>
2018-07-05 15:21:44 -0300

    Adicionar formulario e estilizar -WorkInProgress

--- src/components/FormularioCadastro/FormularioCadastro.js
+++ src/components/FormularioCadastro/FormularioCadastro.js
@@ -4,8 +4,33 @@ import './FormularioCadastro.css';
 class FormularioCadastro extends Component {
   render(){
     return(
-      <div>
-        <h1>Estou no Cadastro</h1>
+      <div className="formulario-cadastro center">
+        <div className="container">
+          <h2>Inscreva-se com suas redes sociais</h2>
+          <button className="social-button">
+            <i className="fab fa-facebook-square"></i>
+            <span>Cadastrar com o Facebook</span>
+          </button>
+          <button className="social-button">
+            <i className="fab fa-google-plus-square"></i>
+            <span>Cadastrar com o Google</span>
+          </button>
+          <h2>Inscreva-se com seu E-mail</h2>
+          <form className="form-cadastro">
+            <input type="email" placeholder="Digite seu e-mail" />
+            <input type="text" placeholder="Primeiro Nome"/>
+            <input type="text" placeholder="Sobrenome"/>
+            <div>
+              <input type="text" placeholder="Dia"/>
+              <input type="text" placeholder="Mês"/>
+              <input type="text" placeholder="Ano"/>
+            </div>
+            <p>Os outros usuários não verão a sua data de nascimento.</p>
+            <input type="password" placeholder="Digite sua senha" />
+            <p>Use no minímo 6 caracteres</p>
+            <button className="social-button">Cadastrar</button>
+          </form>
+        </div>
       </div>
     );
   }
--- src/components/FormularioLogin/FormularioLogin.js
+++ src/components/FormularioLogin/FormularioLogin.js
@@ -4,21 +4,23 @@ import './FormularioLogin.css';
 class FormularioLogin extends Component {
   render() {
     return (
-      <div className="formulario-login">
+      <div className="formulario-login center">
         <div className="container">
           <h2>Já tem uma conta? Faça seu login</h2>
-          <button>
+          <button className="social-button">
             <i className="fab fa-facebook-square"></i>
             <span>Entrar com o Facebook</span>
           </button>
-          <button>
+          <button className="social-button">
             <i className="fab fa-google-plus-square"></i>
             <span>Entrar com o Google</span>
           </button>
-          <p>ou</p>
-          <input type="email" placeholder="Digite seu e-mail" />
-          <input type="password" placeholder="Digite sua senha" />
-          <button>Entrar</button>
+          <h3>ou</h3>
+          <form className="form-login">
+            <input type="email" placeholder="Digite seu e-mail" />
+            <input type="password" placeholder="Digite sua senha" />
+            <button className="social-button">Entrar</button>
+          </form>
         </div>
       </div>
     );
